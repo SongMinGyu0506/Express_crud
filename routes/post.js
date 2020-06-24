423b2f22e5531f7670b8eb8ce7eff313ae0a1696
songmingyu0506 <ggp04114@naver.com>
2020-06-24 23:35:44 +0900

    make create, read

--- routes/post.js
+++ routes/post.js
@@ -3,10 +3,29 @@ const mysql = require('mysql');
 const bodyParser = require('body-parser');
 const router = express.Router();
 
+const connection = mysql.createConnection({
+	host: 'localhost',
+	post: '3306',
+	user: 'root',
+	password: '',
+	database: 'cruddb',
+	multipleStatements: true
+});
+
+connection.connect();
+
 router.use(bodyParser.urlencoded({extended:false}));
 
 router.get('/',(req,res)=>{
-	res.send("/ => TEST PAGE");
+	res.redirect('/post/list');
+});
+
+router.get('/list',(req,res)=>{
+	var sql = "SELECT BRDNO, title, writer, memo, dt FROM tbl_board;";
+	connection.query(sql,(err,rows)=>{
+		if (err) console.error("err: "+err);
+		res.render('list',{rows:rows?rows:{}});
+	});
 });
 
 router.get('/create',(req,res)=>{
@@ -14,9 +33,12 @@ router.get('/create',(req,res)=>{
 });
 
 router.post('/create',(req,res)=> {
-	var testname = req.body.testname;
-	res.send(testname);
-	console.log(testname);
+	var data = [req.body.title, req.body.writer, req.body.memo];
+	var sql = "INSERT INTO tbl_board(title,writer,memo,dt) VALUES(?,?,?,now())";
+	connection.query(sql,data,(err,rows)=> {
+		if(err) console.error("err: " + err);
+});
+	res.redirect('/post/list');
 });
 
 module.exports = router;
